7ad9ff662cc54d742d78f261bdf4457bfda49f3b
Abbas Anwar <abbas.a.anwar@gmail.com>
2025-12-04 20:50:17 -0800

    Initial Commit

---
+++ Khutbah Notes AI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Khutbah Notes AI
+//
+//  Created by Abbas Anwar on 12/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Khutbah Notes AI/Khutbah_Notes_AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Khutbah_Notes_AIApp.swift
+//  Khutbah Notes AI
+//
+//  Created by Abbas Anwar on 12/4/25.
+//
+
+import SwiftUI
+
+@main
+struct Khutbah_Notes_AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
